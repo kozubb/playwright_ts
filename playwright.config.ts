dfc941cebcba5f6188311f06ef600bf881321244
kozubb <piotrek.kozubb@gmail.com>
2025-11-29 22:33:11 +0100

    fix path to reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,9 +21,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
-  reporter: [["junit", { outputFile: "results.xml" }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+  reporter: [["junit", { outputFile: "playwright-report/results.xml" }]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */ use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
