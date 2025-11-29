4008edbb24ef808c94194c77e8a96fcfc3e615a7
kozubb <piotrek.kozubb@gmail.com>
2025-11-29 22:44:10 +0100

    add step to create a folder in yml, add more config

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
-  reporter: [["junit", { outputFile: "playwright-report/results.xml" }]],
+  reporter: [
+    ["line"],
+    ["junit", { outputFile: "playwright-report/results.xml" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */ use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
@@ -35,7 +38,12 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        video: "off",
+        screenshot: "only-on-failure",
+        trace: "retain-on-failure",
+      },
     },
 
     /* Test against mobile viewports. */
