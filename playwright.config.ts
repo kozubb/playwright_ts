2c54dccb34d6956cecaa01755abb0b5219576b94
kozubb <piotrek.kozubb@gmail.com>
2025-11-30 17:13:11 +0100

    fix for github annotations

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,18 +25,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
   reporter: [
-    ["line"],
-    [
-      "junit",
-      { outputFile: path.join(process.cwd(), "playwright-report/results.xml") },
-    ],
-    [
-      "html",
-      {
-        outputFolder: path.join(process.cwd(), "playwright-report/html"),
-        open: "never",
-      },
-    ],
+    ["html", { outputFolder: "playwright-report/html", open: "never" }],
+    ["junit", { outputFile: "playwright-report/results.xml" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */ use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
