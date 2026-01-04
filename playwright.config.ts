53390deeb80ae3ea19727e15430e7b298e9a1049
kozubb <piotrek.kozubb@gmail.com>
2026-01-04 17:07:32 +0100

    add diff pixel ration for CI visual tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,47 +1,48 @@
-import { defineConfig, devices } from "@playwright/test";
-import path from "path";
+import { defineConfig, devices } from '@playwright/test'
+import path from 'path'
 
 export default defineConfig({
-  testDir: "./tests",
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 2 : undefined,
-  timeout: 15 * 1000,
-  expect: {
-    timeout: 5000,
-  },
+	testDir: './tests',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 1 : 0,
+	workers: process.env.CI ? 2 : undefined,
+	timeout: 15 * 1000,
+	expect: {
+		timeout: 5000,
+		toHaveScreenshot: {
+			animations: 'disabled',
+			maxDiffPixelRatio: 0.1
+		}
+	},
 
-  reporter: [
-    ["line"],
-    [
-      "junit",
-      { outputFile: path.join(process.cwd(), "playwright-report/results.xml") },
-    ],
-    [
-      "html",
-      {
-        outputFolder: path.join(process.cwd(), "playwright-report/html"),
-        open: "never",
-      },
-    ],
-    ["@estruyf/github-actions-reporter", { useDetails: true }],
-  ],
-  use: {
-    trace: "on-first-retry",
-    headless: process.env.CI === "true", // headless if in CI environment
-  },
+	reporter: [
+		['line'],
+		['junit', { outputFile: path.join(process.cwd(), 'playwright-report/results.xml') }],
+		[
+			'html',
+			{
+				outputFolder: path.join(process.cwd(), 'playwright-report/html'),
+				open: 'never'
+			}
+		],
+		['@estruyf/github-actions-reporter', { useDetails: true }]
+	],
+	use: {
+		trace: 'on-first-retry',
+		headless: process.env.CI === 'true' // headless if in CI environment
+	},
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: {
-        ...devices["Desktop Chrome"],
-        video: "off",
-        screenshot: "only-on-failure",
-        trace: "retain-on-failure",
-      },
-    },
-  ],
-});
+	/* Configure projects for major browsers */
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+				video: 'off',
+				screenshot: 'only-on-failure',
+				trace: 'retain-on-failure'
+			}
+		}
+	]
+})
